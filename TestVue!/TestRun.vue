021bcfc171e670901b36a80c9cc9cc98fe2bb5b6
TylerBo12 <Tylershovel@gmail.com>
2023-03-07 13:17:46 -0500

    I learn.... stuff!

--- TestVue!/TestRun.vue
+++ TestVue!/TestRun.vue
@@ -1,9 +1,20 @@
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body></body>
-</html>
+<template>
+  <div>
+    <h1>Lopskdapodkaposdp</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+*,
+body,
+html {
+  box-sizing: border-box;
+}
+h1 {
+  font-size: 10rem;
+}
+</style>
