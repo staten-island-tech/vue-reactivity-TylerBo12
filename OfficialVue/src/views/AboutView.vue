71e069da48218b4bc7a4030a0baa4365af37ae1f
TylerBo12 <Tylershovel@gmail.com>
2023-03-08 13:14:26 -0500

    AHHhahahaha
    I'm am in confusion

---
+++ OfficialVue/src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      text: `Trooooll `,
+      hehe: `trolling`,
+      count: 0,
+    };
+  },
+  methods: {
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 v-bind:class="hehe">{{ text }}</h1>
+  <h1>
+    <button @click="decrement">`Click This`</button>
+  </h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 5rem;
+  color: green;
+}
+
+button {
+}
+
+.trolling {
+  color: red;
+}
+</style>
