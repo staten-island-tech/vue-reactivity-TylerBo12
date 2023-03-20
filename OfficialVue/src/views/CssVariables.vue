9f745d73f809583ba9cb064d64c8cbc8db688f37
TylerBo12 <Tylershovel@gmail.com>
2023-03-20 13:15:32 -0400

    I made Cards!!
    Thank you Yifan again

---
+++ OfficialVue/src/views/CssVariables.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+:root {
+  --cardColor: #eaf1f3;
+  --cardBorder: #d31a78;
+  --siteBacco: #df8919;
+  --altsiteBacco: #431a54;
+}
+</style>
